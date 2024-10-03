59cd47c220cae6e27c6953a9d7b63800d61396cf
Victoro2304 <victor.encinas244821@potros.itson.edu.mx>
2024-10-02 18:08:57 -0700

    Cambio de proyecto presentacion

--- ProyectoListaCompraPersistencia/src/main/java/Entidades/Compra.java
+++ ProyectoListaCompraPersistencia/src/main/java/Entidades/Compra.java
@@ -3,6 +3,7 @@ package Entidades;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,7 +23,7 @@ public class Compra implements Serializable {
     @OneToMany(mappedBy = "compra")
     private List<Producto> productos;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "cliente_id", referencedColumnName = "id")
     private Cliente cliente;
 
